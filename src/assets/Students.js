3bf661f258d2005d6e93096f0a3ae65b9a0d5f40
Le Trong Hung <letronghung@users.noreply.github.com>
2019-11-26 01:32:06 +0700

    Code bài tập lớn

---
+++ src/assets/Students.js
@@ -0,0 +1,42 @@
+﻿[{
+        "username": "radicalredemption",
+        "password": "Radicalredemption1",
+        "fullname": "Radical Redemption",
+        "email": "radicalredemption@gmail.com",
+        "gender": "true",
+        "birthday": "1995-12-21",
+        "schoolfee": "1500000",
+        "marks": "0"
+    },
+    {
+        "username": "actofrage",
+        "password": "Actofrage1",
+        "fullname": "Act Of Rage",
+        "email": "actofrage@gmail.com",
+        "gender": "true",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "subzeroproject",
+        "password": "Subzeroproject1",
+        "fullname": "Sub Zero Project",
+        "email": "szp@gmail.com",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    },
+
+    {
+        "username": "letronghung68",
+        "password": "Letronghung68",
+        "fullname": "Lê Trọng Hưng",
+        "email": "lth68@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "2000-08-06",
+        "schoolfee": "6800000",
+        "marks": "0"
+    }
+]
